ef4178b86579ab1a899452601bef3b35465b0d95
Tom Yu <xuejianyuandroid@gmail.com>
2019-12-27 17:42:40 -0600

    First Commit

---
+++ Assets/Ninja Game/Scripts/Ninja.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninja : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+    public float jumpForce = 500;
+
+    bool canJump = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            // Face Left
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            // Face Right
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Move Left
+            transform.position += Vector3.left * Time.deltaTime * speed;
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            // Move Right
+            transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jump();
+        }
+    }
+
+    void jump()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            GetComponent<Rigidbody2D>().AddForce(this.gameObject.transform.up * jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collidingObject)
+    {
+        canJump = true;
+    }
+
+}
