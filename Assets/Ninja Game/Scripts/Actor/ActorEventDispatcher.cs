f62284854b3416542f4da44f2e9f026f3ed87e69
Tom Yu <xuejianyuandroid@gmail.com>
2020-01-01 23:18:19 -0600

    Added Event Dispatcher

---
+++ Assets/Ninja Game/Scripts/Actor/ActorEventDispatcher.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorEventDispatcher : MonoBehaviour {
+
+    public static ActorEventDispatcher I;
+
+    List<Event_Base> events;
+    Event_Base currEvent;
+
+    bool isEventOccuring;
+    bool isEventProcessing;
+
+    void Awake() {
+        I = this;
+
+        events = new List<Event_Base>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            ButtonFullScreen();
+        }
+    }
+
+    public void AddEvent(Event_Base newEvent) {
+        events.Add(newEvent);
+
+        if (!isEventOccuring) {
+            ProcessNextEvent();
+        }
+    }
+
+    void ProcessNextEvent() {
+        StopAllCoroutines();
+        if (events.Count == 0) {
+            isEventOccuring = false;
+            return;
+        }
+
+        isEventOccuring = true;
+        currEvent = events[0];
+        events.RemoveAt(0);
+
+        StartCoroutine(ProcessingCoroutine(currEvent));
+    }
+
+    IEnumerator ProcessingCoroutine(Event_Base _currEvent) {
+        isEventProcessing = true;
+        yield return _currEvent.ProcessCoroutine();
+        isEventProcessing = false;
+
+        StartCoroutine(ProcessCompletedCoroutine(currEvent));
+    }
+
+    IEnumerator ProcessCompletedCoroutine(Event_Base _currEvent) {
+        _currEvent.ProcessComplete();
+        yield return new WaitForSeconds(_currEvent.GetDuration());
+
+        if (events.Count == 0) {
+            isEventOccuring = false;
+        }
+        else {
+            _currEvent.CleanUp();
+            ProcessNextEvent();
+        }
+    }
+
+    public void Clear() {
+        events.Clear();
+        StopAllCoroutines();
+        isEventOccuring = false;
+        isEventProcessing = false;
+    }
+
+    public bool AreAllEventsFinished() {
+        return events.Count == 0 && !isEventOccuring && !isEventProcessing;
+    }
+
+    public void TurnButtonOff() {
+        transform.Find("Button Full Screen").gameObject.SetActive(false);
+    }
+
+    public void TurnButtonOn() {
+        transform.Find("Button Full Screen").gameObject.SetActive(true);
+    }
+
+    // ==================
+    // Button Callbacks
+    // ==================
+    public void ButtonFullScreen() {
+        if (events.Count == 0 && !isEventProcessing) {
+            return;
+        }
+
+        if (currEvent != null && !currEvent.IsSkippable()) {
+            return;
+        }
+
+        if (isEventProcessing) {
+            isEventProcessing = false;
+            StopAllCoroutines();
+            StartCoroutine(ProcessCompletedCoroutine(currEvent));
+        }
+        else {
+            StopAllCoroutines();
+            currEvent.CleanUp();
+            ProcessNextEvent();
+        }
+    }
+}
---
+++ Assets/Ninja Game/Scripts/Characters/MaskedNinja.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class MaskedNinja : MonoBehaviour {
+
+    bool isTriggered;
+    bool shouldPlayCutscene = true;
+
+    void Update() {
+        if (isTriggered && Input.GetKeyDown(KeyCode.RightShift) && shouldPlayCutscene) {
+            PlayCutscene();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            isTriggered = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            isTriggered = false;
+        }
+    }
+
+    private void PlayCutscene() {
+        shouldPlayCutscene = false;
+        AddEvent(new EventSpeech(gameObject, "Hello World"));
+        AddEvent(new EventSpeech(gameObject, "Goodbye World"));
+    }
+
+    void AddEvent(Event_Base _event) {
+        ActorEventDispatcher.I.AddEvent(_event);
+    }
+}
