b448ab7d99a4f2b6d372617a0ef59edcf15b9d45
Tom Yu <xuejianyuandroid@gmail.com>
2020-01-01 20:07:07 -0600

    Added GeneShowSpeechBubble

---
+++ Assets/Ninja Game/Scripts/Gene/GeneShowSpeechBubble.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class GeneShowSpeechBubble : MonoBehaviour {
+
+    public string speech;
+
+    GameObject goSpeechBubble;
+
+    bool isTriggered;
+
+    void Update() {
+        if (isTriggered && Input.GetKeyDown(KeyCode.RightShift) && goSpeechBubble == null) {
+            goSpeechBubble = Toolshed.AddSpeechBubble(speech, transform);
+        }
+        else if (isTriggered && Input.GetKeyDown(KeyCode.RightShift) && goSpeechBubble != null) {
+            CleanUp();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            if (speech == null) {
+                speech = "";
+            }
+
+            isTriggered = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.tag == "Player") {
+            isTriggered = false;
+        }
+        CleanUp();
+    }
+
+    void CleanUp() {
+        Destroy(goSpeechBubble);
+        goSpeechBubble = null;
+    }
+}
--- Assets/Ninja Game/Scripts/_MasterScript.cs
+++ Assets/Ninja Game/Scripts/_MasterScript.cs
@@ -6,7 +6,7 @@ public class _MasterScript : MonoBehaviour {
     // Start is called before the first frame update
     void Start() {
         ActorMusicManager.I.PlayInstant(0);
-        Toolshed.AddSpeechBubble("Hello World", Ninja.I.transform);
+        //Toolshed.AddSpeechBubble("Hello World", Ninja.I.transform);
     }
 
     // Update is called once per frame
